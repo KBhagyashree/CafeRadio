5b2769157089af5f03584d9ebf0432956b46b558
Bhagyashree Khairnar <bhagyashree_khairnar@selinc.com>
2016-08-18 15:18:04 -0700

    created very simple constructors and destructors for base and derived classes, not compiled at this point

--- src/orders/home_delivery.cpp
+++ src/orders/home_delivery.cpp
@@ -1,2 +1,21 @@
-// TODO: Define the HomeDelivery derived class methods
+// Description: Class HomeDelivery --> Derived class contains specific
+// information pertaining to a home delivery order. See order.h for the base
+// class Order.
 //
+// Created by: Bhagyashree Khairnar
+
+
+#include "iostream.h";
+
+class HomeDelivery: public Order
+{
+   public:
+      HomeDelivery(void){
+         cout << "Processing Home Delivery request.";
+      }
+      ~HomeDelivery(void){
+         cout << "Food delivered!";
+      }
+};
+
+// End of File
--- src/orders/order.cpp
+++ src/orders/order.cpp
@@ -1,2 +1,20 @@
-// TODO: Create the order base class
+// Description: Class Order --> This class contains all the common information
+// pertaining to different types of orders namely "To go", "Home delivery", "In
+// house", "Drive through".
 //
+// Created by: Bhagyashree Khairnar
+
+#include "iostream.h";
+
+class Order
+{
+   public:
+      Order(void){
+         cout << "Creating a new Order";
+      }
+      ~Order(void){
+         cout << "Order completed!";
+      }
+};
+
+// End of File
