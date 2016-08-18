7fd1de7dac1154a0c7a822fee3fcbae3dd076be6
Bhagyashree Khairnar <bhagyashree_khairnar@selinc.com>
2016-08-18 15:20:07 -0700

    simple headers ready

--- include/orders/home_delivery.h
+++ include/orders/home_delivery.h
@@ -1,2 +1,22 @@
-// TODO: Declare the HomeDelivery derived class methods and members
+// Description: Class HomeDelivery --> Derived class contains specific
+// information pertaining to a home delivery order. See order.h for the base
+// class Order.
 //
+// Created by: Bhagyashree Khairnar
+
+
+class HomeDelivery: public Order
+{
+   public:
+      HomeDelivery(void);  // Default constructor
+      ~HomeDelivery(void); // Destructor
+
+ // TODO: To be used in latter exercises
+ //  private:
+ //     uint8_t deliveryLocation;
+ //     string driverForDelivery;
+ //     float  estimatedDeliveryTimeMins;
+
+};
+
+// End of File
--- include/orders/order.h
+++ include/orders/order.h
@@ -1,2 +1,29 @@
-// TODO: Create the order base class header
+// Description: Class Order --> This class contains all the common information
+// pertaining to different types of orders namely "To go", "Home delivery", "In
+// house", "Drive through".
 //
+// Created by: Bhagyashree Khairnar
+
+
+// TODO: To be used in latter exercises
+// #define NUM_MAX_ORDERABLE_ITEMS 100
+
+class Order
+{
+   public:
+      Order(void);   // Default Constructor
+      ~Order(void);  // Destructor
+
+  // TODO: To be used in latter exercises
+  // protected:
+  //    // NOTE: A user can place, at the maximum, 256 orders at a time.
+  //    // IDs of completed orders are re-usable.
+  //    uint8_t orderID;
+
+  //    uint8_t orderStatus;
+  //    uint8_t orderType;
+  //    uint8_t orderedItems[NUM_MAX_ORDERABLE_ITEMS];
+
+};
+
+// End of File
